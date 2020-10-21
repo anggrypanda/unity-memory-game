009aa65735a2b22233c8e9813eec1d993bc84c64
AnGGry Panda <madalina.haulica@gmail.com>
2020-10-21 15:26:54 +0300

    Layed out the game buttons and added background images for them

--- Memory Game/Assets/Scripts/AddButtons.cs
+++ Memory Game/Assets/Scripts/AddButtons.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AddButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform gameField;
+    // alternative for GetComponent
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private GameObject btn;
+
+    void Awake()
     {
-        
+        for(int i = 1; i <= 8; i ++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(gameField, false);
+        }
     }
 }
---
+++ Memory Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite bgImage;
+
+    public List<Button> btns = new List<Button>();
+    // created the button list
+
+    void Start()
+    {
+        GetButtons();
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("GameButton");
+        for(int i = 0; i < objects.Length; i ++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+}
